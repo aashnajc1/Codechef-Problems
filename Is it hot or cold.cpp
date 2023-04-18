9851d8ee88d565abc04d94a339522729dccd88c4
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-04-18 23:47:51 +0530

    ,

---
+++ Is it hot or cold.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int c;
+	    cin >> c;
+	    if(c>20){
+	        cout << "hot" << endl;
+	    }
+	    else{
+	        cout << "cold" <<endl;
+	    }
+	}
+	return 0;
+}
