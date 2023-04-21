6cac735b2f6f75471e915075416b692c177d6959
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-04-21 16:10:52 +0530

    ,

---
+++ Fill the Bucket.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int k,y;
+	    cin >> k >> y;
+	    int op = k-y;
+	    cout << op << endl;
+	}
+	return 0;
+}
