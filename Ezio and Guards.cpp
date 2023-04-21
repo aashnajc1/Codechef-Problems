72a69df6b527d60caa8437545f62ef64704a45c7
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-04-22 00:10:49 +0530

    ,

---
+++ Ezio and Guards.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y;
+	    cin >> x >> y;
+	    if(x>=y){
+	        cout << "yes" << endl;
+	    }
+	    else{
+	        cout << "no" << endl;
+	    }
+	}
+	return 0;
+}
