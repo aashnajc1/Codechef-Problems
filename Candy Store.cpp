97cf3448357388242171cd0ce27d627bb18177ab
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-04-22 00:18:56 +0530

    ,

---
+++ Candy Store.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y;
+	    cin >> x >> y;
+	    int cost;
+	    if(x>=y){
+	       cost = y*1;
+	    }
+	    else{
+	        cost = (y-x)*2 + x;
+	    }
+	    cout << cost << endl;
+	}
+	return 0;
+}
