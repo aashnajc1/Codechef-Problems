1958f1f49784f7aa2446e78a2ab5c49ad83c1727
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-04-25 23:30:44 +0530

    ,

---
+++ Expiring Bread.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,m,k;
+	    cin >> n >> m >> k;
+	    if(m*k >= n){
+	        cout << "yes" << endl;
+	    }
+	    else{
+	        cout << "no" << endl;
+	    }
+	}
+	return 0;
+}
