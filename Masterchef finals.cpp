6dfd5cb9ebcb1dfa723d05d05f1cee67d3cc5f15
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-05-14 11:44:01 +0530

    ,

---
+++ Masterchef finals.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    if(x<=10){
+	        cout << "yes" << endl;
+	    }
+	    else{
+	        cout << "no" << endl;
+	    }
+	}
+	return 0;
+}
