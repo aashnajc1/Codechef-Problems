cf5445c27cbe386a3128e2429fa5a7ace9ec5b93
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-06-16 18:44:36 +0530

    ,

---
+++ Learning SQL.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int r,c,e;
+	cin >> r >> c >> e;
+	int extra = r+e;
+	int res = extra * c;
+	cout << res;
+	return 0;
+}
