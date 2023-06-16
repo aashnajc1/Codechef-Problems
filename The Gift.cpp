386d99dea971499a2552fcf42dbea0cf8c372527
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-06-17 01:34:32 +0530

    ,

---
+++ The Gift.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,m,n;
+	cin >> x >> m >> n;
+	if(x+n >= m){
+	    cout << "yes";
+	}
+	else{
+	    cout << "no";
+	}
+	return 0;
+}
